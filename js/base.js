30b6ab6d039eb34a3e78016c2b933d677c0beabf
Ackernym <alexack96@gmail.com>
2024-11-08 19:37:56 -0600

    implemented basic lightbox modal opening

--- js/base.js
+++ js/base.js
@@ -8,25 +8,57 @@ var imageFull = document.createElement("img");
 
 
 //for loop that creates lightbox divs
+//and also changes the hrefs on the image-link class
+//to be #portfolio-image loop number
+//also adds ids to each of the newly created lightbox divs
 function createLightboxDiv() {
   for (let i = 0; i < imagesLightbox.length; i++) {
     console.log(imagesLightbox[i].href);
-    lbContainer.setAttribute("id", `test${[i]}`);
+    lbContainer.setAttribute("id", `image${[i]}`);
     lbContainer.append(imageFull);
     imageFull.src = imagesLightbox[i].href;
     document.body.appendChild(lbContainer.cloneNode(true));
-    imagesLightbox[i].href = "#";
+    imagesLightbox[i].href = `#portfolio-image${[i]}`;
   }
   removeClickListener();
 }
 
-//this has to be set to once: true so that only the first click triggers the creation of divs
+//event listener that creates the lightbox divs and then removes the event listener
+//so that it only fires once
 imagesLightbox.forEach(element => {
   element.addEventListener('click', createLightboxDiv);
 });
 
+//remove click listener function to be added into
+//the createLightbox function
 function removeClickListener() {
   imagesLightbox.forEach(element => {
     element.removeEventListener('click', createLightboxDiv);
   });
 }
+
+// TESTING
+
+
+//cycles through each of the new lightbox divs created
+//and then adds the class lightbox-active to the selected 
+//thumbnail
+imagesLightbox.forEach(element =>{
+  element.addEventListener('click', (e) => {
+    const lightboxes = document.querySelectorAll(".lightbox");
+    var portfolioHref = e.currentTarget.getAttribute('href');
+    var newHrefs = portfolioHref.replace("#portfolio-", "");
+    console.log(newHrefs);
+    console.log(lightboxes.length);
+    for (let i = 0; i < lightboxes.length; i++){
+      if (newHrefs === lightboxes[i].id) {
+        lightboxes[i].classList.add("lightbox-active");
+        console.log("true!");
+        return
+      }
+      else if (newHrefs != lightboxes[i].id){
+        console.log("false! continuing...");
+      }
+    }
+  })
+});
