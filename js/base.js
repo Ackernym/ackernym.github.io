6903f1dc27a45196b46ae32d4590a16e0e1dfb1d
Ackernym <alexack96@gmail.com>
2024-11-09 17:22:56 -0600

    basic lightbox functionality (open/close) created

--- js/base.js
+++ js/base.js
@@ -3,18 +3,26 @@ const imageLink = document.querySelectorAll('.image-link');
 const lightbox = document.querySelector('.lightbox');
 const lightboxImage = document.querySelector('.lightbox-image');
 
+
 // functions
 const showLightBox = () => {lightbox.classList.add('active')}
 
+const hideLightBox = () => {lightbox.classList.remove('active')}
+
+const setActiveImage = (image) => {
+  lightboxImage.src = image.dataset.imgfull;
+}
 
 // event listeners
 imageLink.forEach(image => {
   image.addEventListener('click', (e) => {
-    const oldHref = e.currentTarget.getAttribute('href');
-    const newHref = e.currentTarget.removeAttribute('href');
+    e.currentTarget.removeAttribute('href');
     showLightBox()
-    lightboxImage.src = oldHref;
-    console.log(oldHref);    
+    setActiveImage(image)
     })
   })
+
+  lightbox.addEventListener('click', () => {
+    hideLightBox()
+   })
     
